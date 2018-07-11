ddad0fb50d28d1558962529ec7ed1ae3952e2021
syuan-ying <a19091908@gmail.com>
2018-07-11 16:25:57 +0800

    Step10 extend the same part

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Article(models.Model):
     title = models.CharField(max_length=128, unique=True)
     content = models.TextField()
@@ -12,6 +14,7 @@ class Article(models.Model):
     class Meta:
         ordering = ['-pubDateTime']
 
+
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.CharField(max_length=128)
@@ -22,3 +25,5 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['-pubDateTime']
+
+
